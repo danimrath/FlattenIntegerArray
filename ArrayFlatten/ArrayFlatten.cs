59cf43e9fc1839f419b3f71b6f4013716d2d9ee5
danimrath <anthonyawade@gmail.com>
2017-05-26 09:23:48 -0500

    Initial Commit

---
+++ ArrayFlatten/ArrayFlatten.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ArrayFlattenNS
+{
+    //ArrayFlatten allows you to pass multi-
+    public class ArrayFlatten
+    {
+        public int[] FlattenArray (ArrayList anyDepthArray)
+        {
+            int[] flatArray;
+
+            flatArray = FlattenList(anyDepthArray);
+
+            return flatArray;
+        }
+
+        public int[] FlattenArray (Object[] anyDepthArray)
+        {
+            int[] flatArray;
+
+            flatArray = FlattenList(new ArrayList (anyDepthArray));
+
+            return flatArray;
+        }
+
+        //Recursively loop through an ArrayList to create a single dimension Integer array
+        private int[] FlattenList (ArrayList jaggedArray)
+        {
+            List<int> returnList = new List<int>();
+ 
+            foreach (Object member in jaggedArray)
+            {
+                //if the object is a single integer just add it
+                if (member.GetType().Equals(typeof(int)))
+                {
+                    returnList.Add((int)member);
+                }
+                //if the object is an integer array just add it
+                else if (member.GetType().Equals(typeof(int[])))
+                {
+                    returnList.AddRange((int[])member);
+                }
+                //if the object is an array of objects recursively call FlattenList on it
+                else if (member.GetType().Equals(typeof(Object[])))
+                {
+                    returnList.AddRange(FlattenList(new ArrayList((Object[])member)));
+                }
+                //if the object is an ArrayList recursively call FlattenList on it
+                else if (member.GetType().Equals(typeof(ArrayList)))
+                {
+                    returnList.AddRange(FlattenList((ArrayList)member));
+                }
+                //if the object is not an integer or an array throw an exception
+                else
+                {
+                    throw new System.ArgumentException("Invalid array member " + member, " is not an Integer or an array.");
+                }
+
+            }
+            return returnList.ToArray();
+        }
+    }
+}
---
+++ TestArrayDepth/TestArray.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ArrayFlattenNS;
+using System.Collections;
+
+namespace TestArrayDepth
+{
+    [TestClass]
+    public class TestArray
+    {
+        [TestMethod]
+        public void TestNestedArrayDepth()
+        {
+            ArrayFlatten flattener = new ArrayFlatten();
+            int[] flatArray1;
+            int[] arr = { 1, 2, 3 };
+            int[] arr2 = { 4, 5, 6 };
+            int[] arr3 = { 7, 8, 9 };
+            int[] arr4 = { 10, 11, 12 };
+            Object[] arr5 = { arr, arr2 }; 
+            Object[] arr6 = { arr3, arr4 }; 
+            Object[] arr7 = { 1,arr5, arr6, arr4, arr };
+
+            ArrayList list1 = new ArrayList();
+            list1.Add(arr7);
+            list1.Add(new ArrayList(arr2));
+            flatArray1 = flattener.FlattenArray(list1);
+
+            Assert.IsTrue(flatArray1.Length == 22);
+        }
+        [TestMethod]
+        public void TestMultiDimensionalArray()
+        {
+            ArrayFlatten flattener = new ArrayFlatten();
+            int[] flatArray1;
+            int[][] arr = new int[3][];
+                
+            arr[0] = new int[] { 1, 2, 3 };
+            arr[1] = new int[] { 4, 5, 6 };
+            arr[2] = new int[] { 7, 8, 9 };
+
+            flatArray1 = flattener.FlattenArray(arr);
+
+            Assert.IsTrue(flatArray1.Length == 9);
+        }
+        [TestMethod]
+        public void TestFlatArrayDepth()
+        {
+            ArrayFlatten flattener = new ArrayFlatten();
+            int[] flatArray;
+            int[] arr = { 1, -2, 3 };
+
+            flatArray = flattener.FlattenArray(new ArrayList(arr));
+
+            Assert.IsTrue(flatArray.Length == arr.Length);
+            CollectionAssert.AreEqual(flatArray,arr);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentException))]
+        public void TestArrayMemberException()
+        {
+            ArrayFlatten flattener = new ArrayFlatten();
+            int[] flatArray;
+            int[] arr = { 1, 2, 3 };
+            ArrayList list = new ArrayList();
+            list.Add(arr);
+            list.Add("M");
+
+            flatArray = flattener.FlattenArray(list);
+        }
+    }
+}
